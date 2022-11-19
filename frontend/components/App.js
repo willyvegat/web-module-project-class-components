04ed0ea9bb332d267676b42a50bd1add2641bfc7
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-11-18 22:15:28 -0500

    testing and building the basic work in App, checking for understanding of the assignment

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h2>Todos: </h2>
+        <ul>
+          <li>Take out trash</li>
+          <li>Do laundry</li>
+          <li>Read a chapter</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
