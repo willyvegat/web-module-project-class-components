6b801bff0d251b6ebd585f1a2c1863ca59a1b4a2
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-11-18 22:37:30 -0500

    build this.state and completed message testing

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+      ]
+    }
+  }
+
   render() {
+    
     return (
       <div>
         <h2>Todos: </h2>
         <ul>
-          <li>Take out trash</li>
-          <li>Do laundry</li>
-          <li>Read a chapter</li>
+          {
+            this.state.todos.map(todo => {
+              return (<li>{todo.name} {todo.completed? <span>- completed</span> : <span></span>}</li>)
+            })
+          }
         </ul>
 
         <form>
